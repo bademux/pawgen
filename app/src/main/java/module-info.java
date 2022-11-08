248cf23c4ab2c694fc9ac202b118b75d9b9ba758
bademux <bademux+github@gmail.com>
2022-11-08 23:43:09 +0100

    cleanup dependencies

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,24 +1,25 @@
 module pawgen {
+	requires static lombok;
+
 	exports net.pawet.pawgen;
 	exports net.pawet.pawgen.component;
 	exports net.pawet.pawgen.utils;
 
-	requires static lombok;
 	requires jdk.crypto.ec;
 	requires jdk.crypto.cryptoki;
-	requires java.xml;
 	requires jdk.zipfs;
-	requires com.github.mustachejava;
+	requires java.xml;
 	requires java.desktop;
 	requires java.net.http;
-	requires jakarta.json;
 	requires java.logging;
 	requires org.slf4j;
 	requires org.slf4j.jul;
+	requires com.github.mustachejava;
+	requires com.twelvemonkeys.imageio;
+	requires com.github.jai_imageio;
+	requires jakarta.json;
 	requires org.glassfish.jakarta.json;
 	uses jakarta.json.spi.JsonProvider;
-
-	requires imageio;
 	uses javax.imageio.spi.ImageInputStreamSpi;
 	uses javax.imageio.spi.ImageOutputStreamSpi;
 	uses javax.imageio.spi.ImageWriterSpi;
